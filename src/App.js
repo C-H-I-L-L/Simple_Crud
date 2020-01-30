1a6f326dc9ea2a82d53c3e99ed34e508a299ba94
C-H-I-L-L <fartandgohome@gmail.com>
2020-01-29 17:24:17 -0700

    Played around with event handlers

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onSubmit(event) {
+    event.preventDefault();
+
+    console.log(this.input.value);
+  }
+
+  render() {
+    const list = ['item 1', 'item 2', 'another item'];
+
+    return (
+      <div className='App'>
+        <h1>
+          {list.map(item => {
+            return (
+              <div key={item} onMouseEnter={this.onMouseEnter}>
+                {item}
+              </div>
+            );
+          })}
+        </h1>
+        <form onSubmit={this.onSubmit}>
+          <input onChange={this.onChange} ref={input => (this.input = input)} />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
